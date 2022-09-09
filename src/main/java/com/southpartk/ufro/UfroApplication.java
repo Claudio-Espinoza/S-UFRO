2dd3289675a1165c5c21396507718dd66b7d9532
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2022-09-09 19:09:40 -0300

    Update UfroApplication.java

--- src/main/java/com/southpartk/ufro/UfroApplication.java
+++ src/main/java/com/southpartk/ufro/UfroApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UfroApplication {
+	//hola buenas tardes
 
 	public static void main(String[] args) {
 		SpringApplication.run(UfroApplication.class, args);
