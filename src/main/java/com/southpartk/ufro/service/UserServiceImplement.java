57781545728f63575517cc7fae79709cccec6dbb
Claudio Espinoza Navas <claudioyugen@gmail.com>
2022-09-14 14:32:22 -0300

    Me falto ponerle la etiqueta de servicio. XD

--- src/main/java/com/southpartk/ufro/service/UserServiceImplement.java
+++ src/main/java/com/southpartk/ufro/service/UserServiceImplement.java
@@ -1,12 +1,12 @@
 package com.southpartk.ufro.service;
 
-import com.southpartk.ufro.model.Admin;
 import com.southpartk.ufro.model.User;
 import com.southpartk.ufro.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class UserServiceImplement implements UserService{
     @Autowired
     UserRepository userRepository;
