a6be0120dbf99a9243f2b4c10edd521e21d9b8cb
Claudio Espinoza Navas <claudioyugen@gmail.com>
2022-09-16 16:02:48 -0300

    He agregado la funcionalidad de agregar personajes desde login. Tengo la idea de dejarlo asi el initian, puesto que no estara la opcion de recargar partida

--- src/main/java/com/southpartk/ufro/controller/UsuarioController.java
+++ src/main/java/com/southpartk/ufro/controller/UsuarioController.java
@@ -1,13 +1,21 @@
 package com.southpartk.ufro.controller;
 
+import com.southpartk.ufro.model.User;
+import com.southpartk.ufro.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("index")
 public class UsuarioController {
+    @Autowired
+    private UserService userService;
+    private final Logger LOGGER = Logger.getLogger(String.valueOf(UsuarioController.class));
 
     @GetMapping("")
     public String initian(){
@@ -18,7 +26,14 @@ public class UsuarioController {
     @GetMapping("login")
     public String logIn(){
         System.out.println("Login");
-        return "login/Log-in";
+        return "login/Log-In";
+    }
+
+    @PostMapping("/save")
+    public String save(User user){
+        LOGGER.info("Usuario:  " + user);
+        userService.save(user);
+        return "redirect:/index"; //Esto hay que cambiarlo cuando el juego este listo Owo
     }
 
 
