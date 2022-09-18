c88d0555dcd0d8782c1f0ac98dc0af07c54e6eea
Claudio Espinoza Navas <claudioyugen@gmail.com>
2022-09-18 00:20:08 -0300

    B=========D

--- src/main/java/com/southpartk/ufro/controller/AdminController.java
+++ src/main/java/com/southpartk/ufro/controller/AdminController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.logging.Logger;
 
 @Controller
-@RequestMapping("admin")
+@RequestMapping("")
 public class AdminController {
 
     @Autowired
