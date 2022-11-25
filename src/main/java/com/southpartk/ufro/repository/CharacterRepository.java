a9564899f6b0d760caaaef5f2a71c292e7e08fcb
Claudio Espinoza Navas <claudioyugen@gmail.com>
2022-11-24 23:09:54 -0300

    Debug(BackEnd): RenameClass

--- src/main/java/com/southpartk/ufro/repository/CharacterRepository.java
+++ src/main/java/com/southpartk/ufro/repository/CharacterRepository.java
@@ -1,9 +1,9 @@
 package com.southpartk.ufro.repository;
 
-import com.southpartk.ufro.model.Personage;
+import com.southpartk.ufro.model.Character;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonageRepository extends CrudRepository<Personage, String> {
+public interface CharacterRepository extends CrudRepository<Character, String> {
 }
