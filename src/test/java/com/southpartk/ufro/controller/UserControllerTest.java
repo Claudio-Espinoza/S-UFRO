2f28de417ab98d45b5cb8deae5c14933e501eaf8
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2022-11-24 23:43:06 -0300

    test: Integración de pruebas unitarias
    se integraron pruebas unitarias para el controlador de usuario

--- src/test/java/com/southpartk/ufro/controller/UserControllerTest.java
+++ src/test/java/com/southpartk/ufro/controller/UserControllerTest.java
@@ -1,24 +1,42 @@
 package com.southpartk.ufro.controller;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class UserControllerTest {
+    @BeforeAll
+    static void beforeAll() {;
+    }
+    @AfterAll
+    static void afterAll() {
 
+    }
+    @BeforeEach
+    void setUp() {
+    }
+    @AfterEach
+    void tearDown() {
+
+    }
     @Test
-    void starInitian() {
+    void starInitianRutaTest() {
+        assertEquals("credentials/InitianPage", new UserController().starInitian());
     }
 
     @Test
-    void createCharacter() {
+    void createCharacterRutaTest() {
+        assertEquals("character/DesignCharacter", new UserController().createCharacter());
     }
 
     @Test
     void saveUser() {
+
     }
 
     @Test
     void deleteUser() {
+
     }
 }
