cf35f9977d398aab9d9580fca3595338b538e572
Claudio Espinoza Navas <claudioyugen@gmail.com>
2022-12-05 22:48:22 -0300

    a

---
+++ src/main/java/com/southpartk/ufro/service/PointService.java
@@ -0,0 +1,14 @@
+package com.southpartk.ufro.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PointService {
+
+
+
+    //Esta seccion es mia, alejese perro
+
+
+
+}
